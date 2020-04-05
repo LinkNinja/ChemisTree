8f5be5fd3eee98e544dc1f6d4ecb17062c1da85f
Ecnavda <Ecnavda@gmail.com>
2020-04-05 13:16:26 -0400

    Creating a form to input elements into the database.

---
+++ Chemistree/DBInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chemistree_GUI_V1
+{
+    public partial class DBInput : Form
+    {
+        public DBInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
