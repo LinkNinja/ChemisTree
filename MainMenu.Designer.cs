5d351784bc22ed916c8dc49cb4a52ef3ca4629cb
Bobby Henderson <45887234+BobbyHen@users.noreply.github.com>
2020-04-22 14:29:36 -0400

    Updated buttons in main screen

---
+++ MainMenu.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Chemistree_GUI_V1
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.AppTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.exit_btn = new Chemistree_GUI_V1.RoundedButton();
+            this.roundedButton2 = new Chemistree_GUI_V1.RoundedButton();
+            this.roundedButton1 = new Chemistree_GUI_V1.RoundedButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AppTitle
+            // 
+            this.AppTitle.AutoSize = true;
+            this.AppTitle.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AppTitle.Location = new System.Drawing.Point(454, 67);
+            this.AppTitle.Name = "AppTitle";
+            this.AppTitle.Size = new System.Drawing.Size(174, 36);
+            this.AppTitle.TabIndex = 0;
+            this.AppTitle.Text = "ChemisTree";
+            this.AppTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(476, 113);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 138);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnInput.FlatAppearance.BorderSize = 0;
+            this.btnInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInput.Location = new System.Drawing.Point(16, 487);
+            this.btnInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(100, 28);
+            this.btnInput.TabIndex = 8;
+            this.btnInput.UseVisualStyleBackColor = false;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.exit_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.exit_btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.exit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit_btn.Font = new System.Drawing.Font("Bahnschrift SemiLight", 13.8F);
+            this.exit_btn.ForeColor = System.Drawing.Color.White;
+            this.exit_btn.Location = new System.Drawing.Point(309, 394);
+            this.exit_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(461, 44);
+            this.exit_btn.TabIndex = 7;
+            this.exit_btn.Text = "EXIT";
+            this.exit_btn.UseVisualStyleBackColor = false;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // roundedButton2
+            // 
+            this.roundedButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(255)))), ((int)(((byte)(64)))));
+            this.roundedButton2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.roundedButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton2.Font = new System.Drawing.Font("Bahnschrift Light", 12F);
+            this.roundedButton2.Location = new System.Drawing.Point(309, 340);
+            this.roundedButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.roundedButton2.Name = "roundedButton2";
+            this.roundedButton2.Size = new System.Drawing.Size(461, 44);
+            this.roundedButton2.TabIndex = 6;
+            this.roundedButton2.Text = "FIND ELECTRON CONFIGURATION";
+            this.roundedButton2.UseVisualStyleBackColor = false;
+            this.roundedButton2.Click += new System.EventHandler(this.roundedButton2_Click);
+            // 
+            // roundedButton1
+            // 
+            this.roundedButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(255)))), ((int)(((byte)(64)))));
+            this.roundedButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.roundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton1.Font = new System.Drawing.Font("Bahnschrift Light", 12F);
+            this.roundedButton1.Location = new System.Drawing.Point(309, 285);
+            this.roundedButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.roundedButton1.Name = "roundedButton1";
+            this.roundedButton1.Size = new System.Drawing.Size(461, 44);
+            this.roundedButton1.TabIndex = 5;
+            this.roundedButton1.Text = "CREATE AN IONIC COMPOUND";
+            this.roundedButton1.UseVisualStyleBackColor = false;
+            this.roundedButton1.Click += new System.EventHandler(this.roundedButton1_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1089, 592);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.roundedButton2);
+            this.Controls.Add(this.roundedButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.AppTitle);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChemisTree";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AppTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private RoundedButton roundedButton1;
+        private RoundedButton roundedButton2;
+        private RoundedButton exit_btn;
+        private System.Windows.Forms.Button btnInput;
+    }
+}
+
---
+++ MainMenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chemistree_GUI_V1
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roundedButton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            IoCompoundScreen s2 = new IoCompoundScreen();
+            s2.Show();
+            //this.Close();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            // Close application when exit buton clicked
+            this.Close(); 
+        }
+
+        private void roundedButton2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ElectronConfigScreen s3 = new ElectronConfigScreen();
+            s3.Show();
+            //this.Close();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            DBInput s4 = new DBInput();
+            s4.ShowDialog();
+        }
+    }
+}
